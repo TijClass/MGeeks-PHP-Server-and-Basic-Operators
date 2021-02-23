ce2746a3a86c7bb53852bd3856361dbcd26542ac
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-02-23 16:17:30 +0100

    Amelioration du design

--- Workshop_files(Abdelaziz bardich)/day2/workshop1/tailwind/public/action.php
+++ Workshop_files(Abdelaziz bardich)/day2/workshop1/tailwind/public/action.php
@@ -3,9 +3,9 @@ if(isset($_POST['submit'])){
     $inp = $_POST['input'];
     if($inp == ''){header("location:index.php?res=Unknown error!");die();}
     if($_POST['calc'] == 'c'){
-        $res = ($inp - 32) * 5 / 9;
+        $res = round(($inp - 32) * 5 / 9)." °C";
     }elseif($_POST['calc'] == 'f'){
-        $res = $inp * 9 / 5 + 32;
+        $res = round($inp * 9 / 5 + 32)." °F";
     }else {
         header("location:index.php?res=Wrong operation!");
         die();
--- Workshop_files(Abdelaziz bardich)/day2/workshop1/tailwind/public/index.php
+++ Workshop_files(Abdelaziz bardich)/day2/workshop1/tailwind/public/index.php
@@ -8,22 +8,24 @@
     <link rel="stylesheet" href="./main.css">
 </head>
 <body class="min-h-screen bg-gray-100">
-    <form action="./action.php" method="post" class="m-10 p-5 border rounded text-center">
+    <form action="./action.php" method="post" class="shadow-lg w-6/12 m-auto mt-10 bg-white p-5 border rounded text-center">
         <div>
-            <label for="input" class="block">Input</label>
-            <input type="text" name="input" id="input" class="border rounded w-100">
+            <label for="input" class="block text-xl">Entrez le degré de temperature:</label>
+            <input placeholder="Temperature..." type="number" name="input" id="input" class="w-10/12 m-5 py-2 bg-gray-100 text-black text-center d-block border rounded w-100">
+        </div>
+        <div class="mb-5">
+            <label for="f" class="p-5 w-6 rounded text-white text-center py-2 bg-yellow-500">F</label>
+            <input type="radio" name="calc" id="f" value="f">
         </div>
         <div>
-            <label for="f">F</label>
-            <input type="radio" name="calc" id="f" value="f">
-            <label for="c">C</label>
+            <label for="c" class="p-5 w-6 rounded text-white text-center py-2 bg-yellow-500">C</label>
             <input type="radio" name="calc" id="c" value="c">
         </div>
         <div>
-            <button value="submit" name="submit" class="bg-blue-500 px-10 my-3 rounded" >Send</button>
+            <button value="submit" name="submit" class="bg-blue-900 text-white px-10 w-10/12 py-5 text-xl my-3 rounded" >Send</button>
         </div>
-        <div class="mt-5 border border-5 p-10  b-red flex aling-content-center justify-content-center">
-            Resutl : <?php echo @$_GET['res']; ?>
+        <div class="mt-5 border border-5 p-10  bg-green-300 flex aling-content-center justify-content-center">
+            Resultat : <span class="px-2 rounded bg-blue-900 ml-2 text-white"><?php echo @$_GET['res']; ?></span>
         </div>
     </form>
 </body>
